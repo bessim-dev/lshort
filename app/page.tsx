adf8747e0c2a72e805052e0e48e6b0c252c759d1
bessim-dev <bessim.boujebli@gmail.com>
2022-12-03 21:36:11 +0100

    fetch the list of urls

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
 import Form from "./Form";
-
-export default function Page() {
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+const getShortenedUrls = async () => {
+  const data = await prisma.shortLink.findMany({
+    select: {
+      id: true,
+      url: true,
+      slug: true,
+      createdAt: true,
+    },
+  });
+  return data;
+};
+export default async function Page() {
+  const urls = await getShortenedUrls();
   return (
     <div className="flex flex-col items-center justify-center h-screen border">
       <h1 className="text-6xl font-bold text-[#F9C22E]">Shorten your links</h1>
       <p className="text-2xl font-light">Super Fast Redirections on the edge</p>
       <Form />
+      <ul>
+        {urls.map((url) => (
+          <li key={url.id}>
+            {url.url} - {url.slug}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
